8d20a6952c420ded295548c86f064971cbb4360e
Kayla Dale <kmeyers119@gmail.com>
2024-01-25 22:33:00 -0600

    added getters & setters

--- Animal.java
+++ Animal.java
@@ -1,2 +1,47 @@
 public class Animal {
+    private String type;
+    private String name;
+    private int age;
+    private String habitat;
+    private String makeSound;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMakeSound() {
+        return makeSound;
+    }
+
+    public void setMakeSound(String makeSound) {
+        this.makeSound = makeSound;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
